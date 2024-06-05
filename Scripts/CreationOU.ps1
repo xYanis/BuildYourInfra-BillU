c9f7379fd981d69886c134012fd4955f6db66d28
Alex <161337226+Alex-le-basque@users.noreply.github.com>
2024-06-05 13:11:33 +0200

    Update CreationOU.ps1

--- Scripts/CreationOU.ps1
+++ Scripts/CreationOU.ps1
@@ -1,14 +1,16 @@
-# Import-Module ActiveDirectory (Uncomment this line if the Active Directory module is not already imported)
+# Chemin du fichier CSV
 $csvPath = "path\to\your\CSV_Master.csv"
+# Activer la lignes pour le chemin correspondant
 $baseOU = "OU=BillU-Users,DC=BillU,DC=lan"
+#$baseOU = "OU=BillU-Users,DC=BillU,DC=lan"
 
-# Import the CSV file
+# Importation du fichier CSV
 $csv = Import-Csv -Path $csvPath
 
-# Get unique OU parents, excluding "NA"
+# Unique OU parents, et exclure "NA"
 $uniqueOUs = $csv | Where-Object { $_.OU -ne "NA" } | Select-Object -ExpandProperty OU -Unique
 
-# Loop through each unique OU parent
+# Boucle unique OU parent
 foreach ($ou in $uniqueOUs) {
     $ouPath = "OU=$ou,$baseOU"
     # Check if the OU already exists, if not, create it
@@ -19,13 +21,13 @@ foreach ($ou in $uniqueOUs) {
         Write-Host "Parent OU already exists: $ou"
     }
 
-    # Get unique child OUs for the current parent OU, excluding "NA"
+    # Unique OU enfant pour l'OU parent, et exclure "NA"
     $uniqueChildOUs = $csv | Where-Object { $_.OU -eq $ou -and $_.SousOU -ne "NA" } | Select-Object -ExpandProperty SousOU -Unique
 
-    # Loop through each child OU and create it under the parent OU
+    # Boucle pour chaque OU enfant et création sous l'OU parent
     foreach ($childOU in $uniqueChildOUs) {
         $childOUPath = "OU=$childOU,$ouPath"
-        # Check if the child OU already exists, if not, create it
+        # Vérification OU enfant existe, sinon le créé
         if (-not (Get-ADOrganizationalUnit -Filter {Name -eq $childOU} -SearchBase $ouPath -ErrorAction SilentlyContinue)) {
             New-ADOrganizationalUnit -Name $childOU -Path $ouPath
             Write-Host "Created child OU: $childOU under parent OU: $ou"
