c73fcb4221fdc3235564fba720b068b923f1483f
Alex <161337226+Alex-le-basque@users.noreply.github.com>
2024-06-05 14:18:11 +0200

    Update Creation_GRP_PC.ps1

--- Scripts/Creation_GRP_PC.ps1
+++ Scripts/Creation_GRP_PC.ps1
@@ -5,6 +5,9 @@ $baseOU = "OU=BillU-Computers,DC=BillU,DC=lan"
 # Importer le fichier CSV
 $csv = Import-Csv -Path $csvPath
 
+# Get unique combinations of OU and Groupe_Departement, excluding rows with "NA"
+$uniqueDeptGroups = $csv | Where-Object { $_.OU -ne "NA" -and $_.Groupe_Departement -ne "NA" } | Select-Object -Property OU, Groupe_Departement -Unique
+
 # Boucle pour chaque combinaison unique d'OU et Groupe_Departement
 foreach ($item in $uniqueDeptGroups) {
     $ou = $item.OU
