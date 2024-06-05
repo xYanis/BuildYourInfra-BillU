6aaecc7166105ca5d570ef81b1f8d6d0b5937271
Alex <161337226+Alex-le-basque@users.noreply.github.com>
2024-06-05 13:00:46 +0200

    Update GestionGRPetOU.ps1

--- Scripts/GestionGRPetOU.ps1
+++ Scripts/GestionGRPetOU.ps1
@@ -10,7 +10,7 @@ if (-not (Get-Module -ListAvailable -Name ImportExcel)) {
 
 #Spécifiez le chemin du fichier Excel d'entrée et du fichier CSV de sortie
 $excelFilePath = "C:\Projet_3\s09_BillU.xlsx"
-$csvFilePath = "C:\Projet_3\CSVtest.csv"
+$csvBillUFilePath = "C:\Projet_3\BillU.csv"
 
 $fichierCSV = "C:\Projet_3\CSV_Master.csv"
 
@@ -18,14 +18,14 @@ $fichierCSV = "C:\Projet_3\CSV_Master.csv"
 #Importez les données du fichier Excel
 $data = Import-Excel -Path $excelFilePath
 
-#Filtrez les lignes où la colonne "société" contient "billu"
+#Filtrez les lignes où la colonne "société" contient "BillU"
 $filteredData = $data | Where-Object { $_.Societe -match "BillU" }
 
 #Exportez les données filtrées en CSV
-$filteredData | Export-Csv -Path $csvFilePath -NoTypeInformation
+$filteredData | Export-Csv -Path $csvBillUFilePath -NoTypeInformation
 
 # Importer les données CSV dans une variable PowerShell
-$data = Import-Csv -Path $csvFilePath
+$data = Import-Csv -Path $csvBillUFilePath
 
 # Ajouter une nouvelle colonne "Groupe_Departement" à la variable $data
 $data | Add-Member -MemberType NoteProperty -Name 'Groupe_Departement' -Value ''
@@ -54,13 +54,14 @@ foreach ($row in $data) {
       "QHSE" { $row.Groupe_Departement = "GRP_QHSE" }
       "Service Commercial" { $row.Groupe_Departement = "GRP_SC" }
       "Service recrutement" { $row.Groupe_Departement = "GRP_RECR" }
+      "" { $row.Groupe_Departement = "NA" }
       default { $row.Groupe_Departement = "" }
   }
 
   # Déterminer la valeur de "Groupe_Service" basée sur "Service"
   switch ($service) {
       "Communication interne" { $row.Groupe_Service = "GRP_CRP_CI" }
-      "Relation Medias" { $row.Groupe_Service = "GRP_CRP_RM" }
+      "Relation Médias" { $row.Groupe_Service = "GRP_CRP_RM" }
       "Gestion des marques" { $row.Groupe_Service = "GRP_CRP_GM" }
       "Protection des données et conformité" { $row.Groupe_Service = "GRP_DJ_DATA" }
       "Propriété intellectuelle" { $row.Groupe_Service = "GRP_DJ_PI" }
@@ -71,10 +72,10 @@ foreach ($row in $data) {
       "Exploitation" { $row.Groupe_Service = "GRP_DSI_EXP" }
       "Administration Systèmes et Réseaux" { $row.Groupe_Service = "GRP_DSI_ADMIN" }
       "Support" { $row.Groupe_Service = "GRP_DSI_SUP" }
-      "Developpement et Intégration" { $row.Groupe_Service = "GRP_DSI_DI" }
+      "Développement et Intégration" { $row.Groupe_Service = "GRP_DSI_DI" }
       "Service Comptabilité" { $row.Groupe_Service = "GRP_FC_COM" }
       "Finance" { $row.Groupe_Service = "GRP_FC_FIN" }
-      "Fiscalite" { $row.Groupe_Service = "GRP_FC_FIS" }
+      "Fiscalité" { $row.Groupe_Service = "GRP_FC_FIS" }
       "Contrôle Qualité" { $row.Groupe_Service = "GRP_QHSE_CQ" }
       "Gestion environnementale" { $row.Groupe_Service = "GRP_QHSE_GE" }
       "Certification" { $row.Groupe_Service = "GRP_QHSE_SER" }
@@ -82,6 +83,7 @@ foreach ($row in $data) {
       "Service achat" { $row.Groupe_Service = "GRP_SC_SA" }
       "ADV" { $row.Groupe_Service = "GRP_SC_ADV" }
       "B2B" { $row.Groupe_Service = "GRP_SC_B2B" }
+      "" { $row.Groupe_Service = "NA" }
       default { $row.Groupe_Service = "" }
   }
 }
@@ -104,13 +106,14 @@ foreach ($row in $data) {
         "QHSE" { $row.OU = "QHSE" }
         "Service Commercial" { $row.OU = "Service-Commercial" }
         "Service recrutement" { $row.OU = "Recrutement" }
+        "" { $row.OU = "NA" }
         default { $row.OU = "" }
     }
 
     # Déterminer la valeur de "SousOU" basée sur "Service"
     switch ($service) {
         "Communication interne" { $row.SousOU = "Communication-Interne" }
-        "Relation Medias" { $row.SousOU = "Relations-Medias" }
+        "Relation Médias" { $row.SousOU = "Relations-Medias" }
         "Gestion des marques" { $row.SousOU = "Gestion-Marques" }
         "Protection des données et conformité" { $row.SousOU = "Protection-DATA" }
         "Propriété intellectuelle" { $row.SousOU = "Prop-Int" }
@@ -121,10 +124,10 @@ foreach ($row in $data) {
         "Exploitation" { $row.SousOU = "Exploitation" }
         "Administration Systèmes et Réseaux" { $row.SousOU = "Admin-Systemes-Reseaux" }
         "Support" { $row.SousOU = "Support" }
-        "Developpement et Intégration" { $row.SousOU = "Developpement-Integration" }
+        "Développement et Intégration" { $row.SousOU = "Developpement-Integration" }
         "Service Comptabilité" { $row.SousOU = "Comptabilite" }
         "Finance" { $row.SousOU = "Finance" }
-        "Fiscalite" { $row.SousOU = "Fiscalite" }
+        "Fiscalité" { $row.SousOU = "Fiscalite" }
         "Contrôle Qualité" { $row.SousOU = "Controle-Qualite" }
         "Gestion environnementale" { $row.SousOU = "Gestion-Environnementale" }
         "Certification" { $row.SousOU = "Certification" }
@@ -132,13 +135,10 @@ foreach ($row in $data) {
         "Service achat" { $row.SousOU = "Service-Achats" }
         "ADV" { $row.SousOU = "ADV" }
         "B2B" { $row.SousOU = "B2B" }
+        "" { $row.SousOU = "NA" }
         default { $row.SousOU = "" }
     }
 }
-
-
-
-
 
 # Exporter les données mis à jour dans un nouveau fichier CSV
 $data | Export-Csv -Path $fichierCSV -NoTypeInformation
