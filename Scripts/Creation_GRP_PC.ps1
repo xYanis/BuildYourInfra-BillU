431dfc25f2c5977dc84c25ea5834d2c76ea533fd
Alex <161337226+Alex-le-basque@users.noreply.github.com>
2024-06-05 13:19:33 +0200

    Update Creation_GRP_PC.ps1

--- Scripts/Creation_GRP_PC.ps1
+++ Scripts/Creation_GRP_PC.ps1
@@ -1,21 +1,18 @@
-# Import-Module ActiveDirectory (Uncomment this line if the Active Directory module is not already imported)
+Import-Module ActiveDirectory #(commenter si le module Active Directory est déjà importer)
 $csvPath = "C:\Users\Administrator\Desktop\CSV_Master.csv"
 $baseOU = "OU=BillU-Computers,DC=BillU,DC=lan"
 
-# Import the CSV file
+# Importer le fichier CSV
 $csv = Import-Csv -Path $csvPath
 
-# Get unique combinations of OU and Groupe_Departement, excluding rows with "NA"
-$uniqueDeptGroups = $csv | Where-Object { $_.OU -ne "NA" -and $_.Groupe_Departement -ne "NA" } | Select-Object -Property OU, Groupe_Departement -Unique
-
-# Loop through each unique combination of OU and Groupe_Departement
+# Boucle pour chaque combinaison unique d'OU et Groupe_Departement
 foreach ($item in $uniqueDeptGroups) {
     $ou = $item.OU
     $groupDept = $item.Groupe_Departement
     $groupPCDept = "PC_" + $item.Groupe_Departement
     $ouPath = "OU=$ou,$baseOU"
     
-    # Check if the group already exists, if not, create it
+    # Vérification si le groupe existe,sinon, création
     if (-not (Get-ADGroup -Filter {Name -eq $groupPCDept} -SearchBase $ouPath -ErrorAction SilentlyContinue)) {
         New-ADGroup -Name "$groupPCDept" -GroupScope Global -GroupCategory Security -Path $ouPath
         Write-Host "Created group: $groupPCDept in OU: $ou"
@@ -24,10 +21,10 @@ foreach ($item in $uniqueDeptGroups) {
     }
 }
 
-# Get unique combinations of SousOU and Groupe_Service, excluding rows with "NA"
+# Boucle pour chaque combinaison unique de SousOU et Groupe_Service, exclu les lignes avec "NA"
 $uniqueServiceGroups = $csv | Where-Object { $_.SousOU -ne "NA" -and $_.Groupe_Service -ne "NA" } | Select-Object -Property OU, SousOU, Groupe_Service -Unique
 
-# Loop through each unique combination of SousOU and Groupe_Service
+#  Boucle pour chaque combinaison unique de SousOU et Groupe_Service
 foreach ($item in $uniqueServiceGroups) {
     $ou = $item.OU
     $sousOU = $item.SousOU
@@ -35,7 +32,7 @@ foreach ($item in $uniqueServiceGroups) {
     $groupPCService = "PC_" + $item.Groupe_Service
     $ouPath = "OU=$sousOU,OU=$ou,$baseOU"
     
-    # Check if the group already exists, if not, create it
+    # Vérification si le groupe existe,sinon, création
     if (-not (Get-ADGroup -Filter {Name -eq $groupPCService} -SearchBase $ouPath -ErrorAction SilentlyContinue)) {
         New-ADGroup -Name $groupPCService -GroupScope Global -GroupCategory Security -Path $ouPath
         Write-Host "Created group: $groupPCService in OU: $sousOU under parent OU: $ou"
