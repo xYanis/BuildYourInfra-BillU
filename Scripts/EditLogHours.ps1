5c0b900cddd099b019579b4d554e8ef547b072d6
Alex <161337226+Alex-le-basque@users.noreply.github.com>
2024-06-06 09:47:25 +0200

    Update EditLogHours.ps1

--- Scripts/EditLogHours.ps1
+++ Scripts/EditLogHours.ps1
@@ -1,47 +1,73 @@
-# Import-Module ActiveDirectory s'il n'est pas déjà importé
-Import-Module ActiveDirectory
-
-# Définir les heures de connexion du lundi au samedi de 7h30 à 20h00
-# Chaque heure est représentée par 2 octets (intervalles de 30 minutes)
-# Il y a 21 intervalles de 7h30 à 20h00 (42 demi-heures) pour chaque jour (42 * 6 jours = 252 intervalles)
-# Total de 336 intervalles par semaine (7 jours * 24 heures * 2 intervalles par heure)
-
-
-$logonHours = @(
-    # Lundi
-    0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-    0,0,0,0,0,0,
-    # Mardi
-    0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-    0,0,0,0,0,0,
-    # Mercredi
-    0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-    0,0,0,0,0,0,
-    # Jeudi
-    0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-    0,0,0,0,0,0,
-    # Vendredi
-    0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-    0,0,0,0,0,0,
-    # Samedi
-    0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-    0,0,0,0,0,0,
-    # Dimanche (tous les zéros, pas d'accès)
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-)
-
-# Convertir le tableau logonHours en tableau d'octets
-$logonHoursBytes = [byte[]]$logonHours
-
-# Obtenez tous les utilisateurs dans BillU-Users
-$users = Get-ADUser -SearchBase "OU=BillU-Users,DC=BillU,DC=lan" -Filter *
-
-# Mettre à jour les heures de connexion pour chaque utilisateur
-foreach ($user in $users) {
-    try {
-        Set-ADUser -Identity $user -Replace @{logonHours = $logonHoursBytes}
-        Write-Output "Updated logon hours for $($user.SamAccountName)"
-    } catch {
-        Write-Output "Failed to update logon hours for $($user.SamAccountName): $_"
+#Fonction pour simplifcation de la gestion horaire (conversion Binaire, selection jour, selection heure, ...)
+Function Set-LogonHours{
+    [CmdletBinding()]
+    Param(
+    [Parameter(Mandatory=$True)][ValidateRange(0,23)]$TimeIn24Format,
+    [Parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True,Position=0)]$Identity,
+    [parameter(mandatory=$False)][ValidateSet("WorkingDays", "NonWorkingDays")]$NonSelectedDaysare="NonWorkingDays",
+    [parameter(mandatory=$false)][switch]$Sunday,
+    [parameter(mandatory=$false)][switch]$Monday,
+    [parameter(mandatory=$false)][switch]$Tuesday,
+    [parameter(mandatory=$false)][switch]$Wednesday,
+    [parameter(mandatory=$false)][switch]$Thursday,
+    [parameter(mandatory=$false)][switch]$Friday,
+    [parameter(mandatory=$false)][switch]$Saturday
+ )
+ Process{
+    $FullByte=New-Object "byte[]" 21
+    $FullDay=[ordered]@{}
+    0..23 | foreach{$FullDay.Add($_,"0")}
+    $TimeIn24Format.ForEach({$FullDay[$_]=1})
+    $Working= -join ($FullDay.values)
+    Switch ($PSBoundParameters["NonSelectedDaysare"])
+    {
+     'NonWorkingDays' {$SundayValue=$MondayValue=$TuesdayValue=$WednesdayValue=$ThursdayValue=$FridayValue=$SaturdayValue="000000000000000000000000"}
+     'WorkingDays' {$SundayValue=$MondayValue=$TuesdayValue=$WednesdayValue=$ThursdayValue=$FridayValue=$SaturdayValue="111111111111111111111111"}
     }
-}
+    Switch ($PSBoundParameters.Keys)
+    {
+     'Sunday' {$SundayValue=$Working}
+     'Monday' {$MondayValue=$Working}
+     'Tuesday' {$TuesdayValue=$Working}
+     'Wednesday' {$WednesdayValue=$Working}
+     'Thursday' {$ThursdayValue=$Working}
+     'Friday' {$FridayValue=$Working}
+     'Saturday' {$SaturdayValue=$Working}
+    }
+ 
+    $AllTheWeek="{0}{1}{2}{3}{4}{5}{6}" -f $SundayValue,$MondayValue,$TuesdayValue,$WednesdayValue,$ThursdayValue,$FridayValue,$SaturdayValue
+ 
+    # Timezone Check
+    if ((Get-TimeZone).baseutcoffset.hours -lt 0){
+     $TimeZoneOffset = $AllTheWeek.Substring(0,168+ ((Get-TimeZone).baseutcoffset.hours))
+     $TimeZoneOffset1 = $AllTheWeek.SubString(168 + ((Get-TimeZone).baseutcoffset.hours))
+     $FixedTimeZoneOffSet="$TimeZoneOffset1$TimeZoneOffset"
+    }
+    if ((Get-TimeZone).baseutcoffset.hours -gt 0){
+     $TimeZoneOffset = $AllTheWeek.Substring(0,((Get-TimeZone).baseutcoffset.hours))
+     $TimeZoneOffset1 = $AllTheWeek.SubString(((Get-TimeZone).baseutcoffset.hours))
+     $FixedTimeZoneOffSet="$TimeZoneOffset1$TimeZoneOffset"
+    }
+    if ((Get-TimeZone).baseutcoffset.hours -eq 0){
+     $FixedTimeZoneOffSet=$AllTheWeek
+    }
+ 
+    $i=0
+    $BinaryResult=$FixedTimeZoneOffSet -split '(\d{8})' | Where {$_ -match '(\d{8})'}
+ 
+    Foreach($singleByte in $BinaryResult){
+     $Tempvar=$singleByte.tochararray()
+     [array]::Reverse($Tempvar)
+     $Tempvar= -join $Tempvar
+     $Byte = [Convert]::ToByte($Tempvar, 2)
+     $FullByte[$i]=$Byte
+     $i++
+    }
+    Set-ADUser -Identity $Identity -Replace @{logonhours = $FullByte} 
+ }
+ end{
+    Write-Host "Les horaires de connexion de tous les utilisateurs ont bien été prise en compte" -ForegroundColor green
+ }
+ }
+ 
+ Get-ADUser -SearchBase "OU=BillU-Users,DC=BillU,DC=lan" -Filter *| Set-LogonHours -TimeIn24Format @(7,8,9,10,11,12,13,14,15,16,17,18,19) -Monday -Tuesday -Wednesday -Thursday -Friday -Saturday -NonSelectedDaysare NonWorkingDays
