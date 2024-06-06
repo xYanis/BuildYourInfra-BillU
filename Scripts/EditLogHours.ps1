e53659e890b6824a3601fcdb5339ccc584d646cf
Alex <161337226+Alex-le-basque@users.noreply.github.com>
2024-06-06 08:02:00 +0200

    Update EditLogHours.ps1

--- Scripts/EditLogHours.ps1
+++ Scripts/EditLogHours.ps1
@@ -26,12 +26,10 @@ $logonHours = @(
 $logonHoursBytes = [byte[]]$logonHours
 
 # Obtenez tous les utilisateurs sauf ceux du groupe "Administrateurs du domaine"
-$users = Get-ADUser -Filter * -SearchBase "OU=BillU-Users,DC=BillU,DC=lan" -Properties MemberOf | Where-Object {
-    $_.MemberOf -notcontains "CN=Domain Admins,CN=Users,DC=BillU,DC=lan"
-}
+$users = Get-ADUser -Filter * -SearchBase "OU=BillU-Users,DC=BillU,DC=lan" -Properties MemberOf #| Where-Object { $_.MemberOf -notcontains "CN=Domain Admins,CN=Users,DC=BillU,DC=lan" }
 
 # Mettre à jour les heures de connexion pour chaque utilisateur
 foreach ($user in $users) {
     Set-ADUser -Identity $user -Replace @{logonHours = $logonHoursBytes}
-    Write-Output "Updated logon hours for $($user.SamAccountName)"
+    Write-Output "Mise à jour logon hours pour $($user.SamAccountName)"
 }
