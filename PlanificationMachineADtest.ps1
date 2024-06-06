122448f5c02dfe225ca9895f5bf6d6e1c7fd87e7
Sherub1 <160050170+Sherub1@users.noreply.github.com>
2024-06-06 13:31:15 +0200

    Create PlanificationMachineADtest.ps1

---
+++ PlanificationMachineADtest.ps1
@@ -0,0 +1,14 @@
+# Dernière écriture sur le fichier CSV_Master
+$lastWriteTime = (Get-Item "C:\Users\Administrator\Desktop\CSV_Master.csv").LastWriteTime
+
+# Wait for 5 seconds
+Start-Sleep -Seconds 5
+
+# Vérifier si le fichier a été modifié
+$currentWriteTime = (Get-Item "C:\Users\Administrator\Desktop\CSV_Master.csv").LastWriteTime
+if ($currentWriteTime -ne $lastWriteTime) {
+    Write-Host "Le fichier a été modifié!"
+# Executer la commande de mise à jour de l'AD
+#-ExecutionPolicy Bypass -File "C:\Users\Administrator\Desktop\scriptADDComputer.ps1"
+
+}
